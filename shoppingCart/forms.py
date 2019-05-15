d5432af4bdf13b651885235affa412842ec0577d
Savin Kumar <savinkumarn@gmail.com>
2019-05-15 09:03:18 +0530

    update 9

--- shoppingCart/forms.py
+++ shoppingCart/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class ItemForm(forms.Form):
     item_number = forms.IntegerField()
-    item_quantity = forms.CharField()
+    item_quantity = forms.IntegerField()
--- shoppingCart/views.py
+++ shoppingCart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import SCart
 from random import randint
 from .items import Items
+from .forms import ItemForm
 # Create your views here.
 
 
@@ -26,6 +27,8 @@ def addOrder(request):
     context = {
         "workfile": generateWorkfile(),
         "cartQty": 0,
+        "items": [],
+        "itemForm": ItemForm(),
     }
     return render(request, 'sCart/addOrder.html', context)
 
@@ -35,9 +38,27 @@ def saveOrder(request):
 
 
 def addItem(request):
-    print(request.POST)
-    context = {
-        "workfile": generateWorkfile(),
-        "cartQty": 0,
-    }
+    context = None
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            print(request.POST)
+            workfile = request.POST.get('workfile')
+            cartQty = int(request.POST.get('cartQty')) + 1
+            items = request.POST.get('items')
+            item_number = request.POST.get('item_number')
+            item_quantity = request.POST.get('item_quantity')
+            dummy_item = {}
+            dummy_item["number"] = item_number
+            dummy_item["quantity"] = item_quantity
+            items_list = items.strip('][').split(', ')
+            items_list.append(dummy_item)
+            print(items_list)
+            context = {
+                "workfile": workfile,
+                "cartQty": cartQty,
+                "items": items_list,
+                "itemForm": ItemForm(),
+            }
+
     return render(request, 'sCart/addOrder.html', context)
